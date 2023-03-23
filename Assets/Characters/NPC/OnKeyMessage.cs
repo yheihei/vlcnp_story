cb2e12762d6c28efb54ce1d76cbbe2bdac2f2e99
yheihei <yhei.register@gmail.com>
2023-03-23 23:20:28 +0900

    お試し実装 - 会話ウィンドウ #16

---
+++ Assets/Characters/NPC/OnKeyMessage.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class OnKeyMessage : MonoBehaviour
+{
+    public Flowchart Flowchart;
+    public string BlockName;
+    private bool isChat = false;
+    private bool isCollision = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.tag.Equals("Player"))
+        {
+            return;
+        }
+        isCollision = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.tag.Equals("Player")) {
+            return;
+        }
+        isCollision = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("up") && !isChat)
+        {
+            isChat = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isCollision) {
+            return;
+        }
+        if (!isChat) {
+            return;
+        }
+        if (Flowchart.HasExecutingBlocks())
+        {
+            return;
+        }
+        Debug.Log("開始");
+        Flowchart.ExecuteBlock(BlockName);
+        isChat = false;
+        Debug.Log("終了");
+    }
+}
