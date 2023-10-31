58c2b1dbc7d962d8f0df46e2aa0e2e2197d1d7c3
yheihei <yhei.register@gmail.com>
2023-10-31 21:32:18 +0900

    イベント開始

---
+++ Assets/Scripts/Movie/LeeleeFukkatu.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using Fungus;
+using Newtonsoft.Json.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Playables;
+using VLCNP.Actions;
+using VLCNP.Control;
+using VLCNP.Core;
+using VLCNP.Saving;
+using VLCNP.UI;
+
+namespace VLCNP.Movie
+{
+    public class LeeleeFukkatu : MonoBehaviour
+    {
+        PlayableDirector playableDirector;
+        Transform startPoint;
+        [SerializeField]
+        public Flowchart flowChart;
+        [SerializeField]
+        public AudioClip audioClip;
+        [SerializeField]
+        public float bgmVolume = 0.3f;
+        [SerializeField]
+        public float bgmPitch = 0.5f;
+
+        FlagManager flagManager;
+
+        private void Awake() {
+            playableDirector = GetComponent<PlayableDirector>();
+            flagManager = GameObject.FindWithTag("FlagManager").GetComponent<FlagManager>();
+        }
+
+        private void OnEnable() {
+            playableDirector.played += DisableControl;
+            playableDirector.stopped += EnableControl;
+        }
+
+        private void OnDisable() {
+            playableDirector.played -= DisableControl;
+            playableDirector.stopped -= EnableControl;
+        }
+
+        void DisableControl(PlayableDirector director) {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+
+        void EnableControl(PlayableDirector director) {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+
+        IEnumerator BGMPlay(float waitTime)
+        {
+            yield return new WaitForSeconds(waitTime);
+            AudioSource audioSource = GameObject.FindWithTag("BGM").GetComponent<AudioSource>();
+            if (audioClip != null)
+            {
+                audioSource.clip = audioClip;
+                audioSource.volume = bgmVolume;
+                audioSource.pitch = bgmPitch;
+                audioSource.Play();
+            }
+        }
+
+        public void Execute()
+        {
+            playableDirector.Play();
+        }
+
+        IEnumerator Talk() {
+            StopAll();
+            flowChart.ExecuteBlock("AkimQuestion");
+            yield return new WaitUntil(() => flowChart.GetExecutingBlocks().Count == 0);
+            StartAll();
+        }
+
+        private void StopAll()
+        {
+            foreach (MonoBehaviour obj in FindObjectsOfType<MonoBehaviour>())
+            {
+                IStoppable stoppable = obj.GetComponent<IStoppable>();
+                if (stoppable == null) continue;
+                stoppable.IsStopped = true;
+            }
+        }
+
+        private void StartAll()
+        {
+            foreach (MonoBehaviour obj in FindObjectsOfType<MonoBehaviour>())
+            {
+                IStoppable stoppable = obj.transform.GetComponent<IStoppable>();
+                if (stoppable == null) continue;
+                stoppable.IsStopped = false;
+            }
+        }
+    }
+}
