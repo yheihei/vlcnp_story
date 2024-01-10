d76c056b3c338b70f4c076233f2dbd73814c6898
yheihei <yhei.register@gmail.com>
2024-01-10 17:59:30 +0900

    リファクタ - カベキック時のエフェクトをつける #199

--- Assets/Scripts/Movement/KabeKickEffectController.cs
+++ Assets/Scripts/Movement/KabeKickEffectController.cs
@@ -34,30 +34,40 @@ namespace VLCNP.Movement
 
         void FixedUpdate()
         {
+            if (!CheckEffecting()) return;
+            InstantiateEffect();
+        }
+
+        bool CheckEffecting()
+        {
             // 壁に接触していなければ何もしない
             if (!isColliding) {
                 effectElapsedTime = 0f;
-                return;
+                return false;
             }
             // x方向に移動していれば壁ではないので何もしない
             if (player.velocity.x != 0f)
             {
                 effectElapsedTime = 0f;
-                return;
+                return false;
             }
             // y方向の速度が一定以上であれば何もしない
             if (player.velocity.y >= -0.05f)
             {
                 effectElapsedTime = 0f;
-                return;
+                return false;
             }
             // エフェクトが最後に発生してからの経過時間がeffectWaitTimeより小さければ何もしない
             if (effectElapsedTime < effectWaitTime)
             {
                 effectElapsedTime += Time.deltaTime;
-                return;
+                return false;
             }
-            // エフェクトを発生させる
+            return true;
+        }
+
+        void InstantiateEffect()
+        {
             Instantiate(effect, transform.position, Quaternion.identity);
             effectElapsedTime = 0f;
         }
