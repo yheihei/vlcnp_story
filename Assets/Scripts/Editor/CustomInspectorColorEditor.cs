3e22b95e53f34b266dda19b3ac1ddfcead6d086c
yheihei <yhei.register@gmail.com>
2024-04-29 12:12:50 +0900

    特定のインターフェースのInspectorの色を変更するスクリプトのリファクタ

--- Assets/Scripts/Editor/CustomInspectorColorEditor.cs
+++ Assets/Scripts/Editor/CustomInspectorColorEditor.cs
@@ -1,18 +1,36 @@
 using UnityEngine;
 using UnityEditor;
 using VLCNP.Combat.EnemyAction;
+using System.Collections.Generic;
+using System;
+
 
 [CustomEditor(typeof(MonoBehaviour), true)]
 public class CustomInspectorColorEditor : Editor
 {
+    private static Dictionary<Type, Color> interfaceColorMapping;
+
+    static CustomInspectorColorEditor()
+    {
+        interfaceColorMapping = new Dictionary<Type, Color>
+        {
+            { typeof(IEnemyAction), Color.blue }
+            // 他のインターフェースと色のマッピングをここに追加
+        };
+    }
+
     public override void OnInspectorGUI()
     {
         MonoBehaviour myTarget = (MonoBehaviour) target;
 
-        if (myTarget is IEnemyAction)
+        // 特定のインターフェースの場合 色を変更
+        foreach (var pair in interfaceColorMapping)
         {
-            // 特定のインターフェースを実装している場合の色変更
-            GUI.backgroundColor = Color.blue;
+            if (myTarget.GetType().GetInterface(pair.Key.Name) != null)
+            {
+                GUI.backgroundColor = pair.Value;
+                break; // 最初に見つかったマッチで色を設定
+            }
         }
 
         // 通常のインスペクターGUIを描画
