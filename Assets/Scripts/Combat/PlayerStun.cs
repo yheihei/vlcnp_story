d21f4417b883f2f81381acd64d918825490083d0
yheihei <yhei.register@gmail.com>
2024-09-17 03:24:38 +0900

    パラメータ調整

--- Assets/Scripts/Combat/PlayerStun.cs
+++ Assets/Scripts/Combat/PlayerStun.cs
@@ -7,7 +7,7 @@ namespace VLCNP.Combat
     public class PlayerStun : MonoBehaviour
     {
         public float stunDuration = 0.5f;
-        public float stunFrame = 120f;
+        public float stunFrame = 30f;
         private float stunRecoveryFrame = 0;
 
         private bool isStunned = false;
