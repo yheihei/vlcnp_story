c88e61217431252229c1f6492fc009cc55ea8784
yheihei <yhei.register@gmail.com>
2025-01-22 16:11:29 +0900

    回復アイテムを作る。敵に低確率でドロップさせる #463

--- Assets/Scripts/Pickups/DropItem.cs
+++ Assets/Scripts/Pickups/DropItem.cs
@@ -5,11 +5,18 @@ namespace VLCNP.Pickups
     public class DropItem : MonoBehaviour, IDropItem
     {
         [SerializeField]
-        GameObject dropItem = null;
+        BasePickup dropItem = null;
 
         [SerializeField]
         float dropRate = 0.2f;
 
-        public void Drop() { }
+        public void Drop()
+        {
+            if (dropItem == null)
+                return;
+            if (Random.value > dropRate)
+                return;
+            Instantiate(dropItem, transform.position, Quaternion.identity);
+        }
     }
 }
