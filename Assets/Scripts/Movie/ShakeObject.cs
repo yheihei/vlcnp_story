ece0d886bb6476b2db4442cd2f4adc8506227575
yheihei <yhei.register@gmail.com>
2025-04-09 17:52:03 +0900

    カルマがVLオロチを復活させるところまで

---
+++ Assets/Scripts/Movie/ShakeObject.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+
+namespace VLCNP.Movie
+{
+    public class ShakeObject : MonoBehaviour
+    {
+        public bool IsStopped { get; private set; } = false;
+
+        public void Shake(float magnitude = 0.1f)
+        {
+            IsStopped = false;
+            StartCoroutine(ShakeCoroutine(magnitude));
+        }
+
+        private IEnumerator ShakeCoroutine(float magnitude)
+        {
+            Vector3 originalPosition = transform.position;
+            float elapsedTime = 0;
+            while (!IsStopped)
+            {
+                transform.position =
+                    originalPosition
+                    + new Vector3(
+                        UnityEngine.Random.Range(-1f, 1f) * magnitude,
+                        UnityEngine.Random.Range(-1f, 1f) * magnitude,
+                        0
+                    );
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.position = originalPosition;
+        }
+
+        public void Stop()
+        {
+            IsStopped = true;
+        }
+    }
+}
